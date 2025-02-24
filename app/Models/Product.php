e435a889e04b9ebbb6f5cd09f6f7c30a6def1310
notmIaa <miaminue93@gmail.com>
2025-02-24 09:02:46 +0800

    Create Product.php

---
+++ app/Models/Product.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Prodcut extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'image',
+        'incubatee_name',
+        'business_name',
+    ];
+
+    public function products()
+    {
+        return $this->hasMany(IncubateeProduct::class, 'incubatee_id');
+    }
+}
